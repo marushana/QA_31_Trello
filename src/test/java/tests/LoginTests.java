fdba8f9e794bd5ffd712a6c0ecd89c542fcdd69e
marushana <marushana@yandex.ru>
2025-11-27 19:25:40 +0200

    Start project

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,11 @@
 package tests;
 
-public class LoginTests {
+import manager.AppManager;
+import org.testng.annotations.Test;
+
+public class LoginTests extends AppManager {
+    @Test
+    public void loginPositiveTest(){
+
+    }
 }
